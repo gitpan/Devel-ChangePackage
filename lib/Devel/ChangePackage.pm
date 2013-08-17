4fbf4b10171c0f61ebf3d1db9b1cf26e6b65625f
Lukas Mai <mauke@cpan.org>
2013-08-17 19:19:41 +0000

    Import of MAUKE/Devel-ChangePackage-0.08 from CPAN.
    gitpan-cpan-distribution: Devel-ChangePackage
    gitpan-cpan-version:      0.08
    gitpan-cpan-path:         MAUKE/Devel-ChangePackage-0.08.tar.gz
    gitpan-cpan-author:       MAUKE
    gitpan-cpan-maturity:     released

--- lib/Devel/ChangePackage.pm
+++ lib/Devel/ChangePackage.pm
@@ -5,8 +5,8 @@ package Devel::ChangePackage;
 BEGIN {
   $Devel::ChangePackage::AUTHORITY = 'cpan:FLORA';
 }
-BEGIN {
-  $Devel::ChangePackage::VERSION = '0.07';
+{
+  $Devel::ChangePackage::VERSION = '0.08';
 }
 # ABSTRACT: Change the package code is currently being compiled in
 
@@ -30,6 +30,7 @@ XSLoader::load(
 1;
 
 __END__
+
 =pod
 
 =encoding utf-8
@@ -125,10 +126,9 @@ Florian Ragwitz <rafl@debian.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2010 by Florian Ragwitz.
+This software is copyright (c) 2013 by Florian Ragwitz.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
 
 =cut
-
